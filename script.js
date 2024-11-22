5be2c8708effd3b1da2ea56066507ec9fb6cdabe
francistops <65456051+francistops@users.noreply.github.com>
2024-11-22 16:59:43 -0500

    homemade rewrite

---
+++ script.js
@@ -0,0 +1,19 @@
+function darkMode() {
+    const darkModeButton = document.getElementById("dark-mode");
+    document.body.classList.toggle("dark-mode");
+
+    if (darkModeButton.innerText === "🌞") {
+        darkModeButton.innerText = "🌛";
+    } else if (darkModeButton.innerText === "🌛") {
+        darkModeButton.innerText = "🌞";
+    }
+}
+
+window.onload = function() {
+    const darkModeButton = document.getElementById("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        darkModeButton.innerText = "🌛";
+    } else {
+        darkModeButton.innerText = "🌞";
+    }
+};
