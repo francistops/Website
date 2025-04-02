9b27b3648205fe3a658fa18a5d7a50f9dc935f2d
francistops <65456051+francistops@users.noreply.github.com>
2025-04-02 16:32:09 -0400

    basic responsive design need work

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
+// disable for now
 function darkMode() {
-    const darkModeButton = document.getElementById("dark-mode");
-    document.body.classList.toggle("dark-mode");
+  const darkModeButton = document.getElementById("dark-mode");
+  document.body.classList.toggle("dark-mode");
 
-    if (darkModeButton.innerText === "🌞") {
-        darkModeButton.innerText = "🌛";
-    } else if (darkModeButton.innerText === "🌛") {
-        darkModeButton.innerText = "🌞";
-    }
+  if (darkModeButton.innerText === "🌞") {
+    darkModeButton.innerText = "🌛";
+  } else if (darkModeButton.innerText === "🌛") {
+    darkModeButton.innerText = "🌞";
+  }
 }
 
-window.onload = function() {
-    const darkModeButton = document.getElementById("dark-mode");
-    if (document.body.classList.contains("dark-mode")) {
-        darkModeButton.innerText = "🌛";
-    } else {
-        darkModeButton.innerText = "🌞";
-    }
+window.onload = function () {
+  const darkModeButton = document.getElementById("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    darkModeButton.innerText = "🌛";
+  } else {
+    darkModeButton.innerText = "🌞";
+  }
 };
